c6d6cce6d1802a2a8c32de4928e297486944d9f7
Daniel Azevedo Marcondes de Souza <n6_daniel@yahoo.com.br>
2022-10-14 14:26:17 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+
+def calc(operator, x, y):
+    cases = {
+        "+": lambda a, b: print(f"{a} + {b} =", a + b),
+        "-": lambda a, b: print(f"{a} - {b} =", a - b),
+        "*": lambda a, b: print(f"{a} * {b} =", a * b),
+        "/": lambda a, b: print(f"{a} / {b} =", a / b),
+        "**": lambda a, b: print(f"{a} ** {b} =", a ** b),
+    }
+    return cases[operator](x, y)
+
+
+def newOp():
+    new = str(input("Deseja fazer outra operação? S/N: "))
+    if new == "S" or new == "s":
+        menu()
+    elif new == "N" or new == "n":
+        if new == "N" or new == 'n':
+            op = str(input("Tem certeza que deseja sair? S/N: "))
+            if op == "S" or op == 's':
+                print("Saindo da calculadora...")
+                sys.exit()
+            else:
+                menu()
+    else:
+        print("Opção inválida!")
+
+
+def menu():
+    op = 1
+    while op:
+        os.system('clear')
+        print('========================================')
+        print("0 : Soma ")
+        print("1 : Subtração ")
+        print("2 : Multiplicação ")
+        print("3 : Divisão ")
+        print("4 : Exponenciação ")
+        print("9 : Sair")
+        print('========================================')
+        op = int(input('| : Escolha sua opção: '))
+
+        if op == 0:
+            print(calc("+", float(input("Priemiro número:\n")), float(input("Segundo número:\n"))))
+            newOp()
+        elif op == 1:
+            print(calc("-", float(input("Priemiro número:\n")), float(input("Segundo número:\n"))))
+            newOp()
+        elif op == 2:
+            print(calc("*", float(input("Priemiro número:\n")), float(input("Segundo número:\n"))))
+            newOp()
+        elif op == 3:
+            print(calc("/", float(input("Priemiro número:\n")), float(input("Segundo número:\n"))))
+            newOp()
+        elif op == 4:
+            print(calc("**", float(input("Priemiro número:\n")), float(input("Segundo número:\n"))))
+            newOp()
+        elif op == 9:
+            op = str(input("Tem certeza que deseja sair? S/N: \n"))
+            if op == "S" or op == 's':
+                print("Saindo da calculadora...")
+                sys.exit()
+            else:
+                menu()
+        else:
+            print("Opção inválida!")
+
+
+menu()
